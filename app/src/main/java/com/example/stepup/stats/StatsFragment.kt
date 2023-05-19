bcbe9647e81ecead9b983875cc9e5e2b48e2fd2e
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-05-20 00:49:49 +0530

    Update StatsFragment

--- app/src/main/java/com/example/stepup/stats/StatsFragment.kt
+++ app/src/main/java/com/example/stepup/stats/StatsFragment.kt
@@ -1,60 +1,53 @@
 package com.example.stepup.stats
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.stepup.R
+import com.example.stepup.databinding.FragmentStatsBinding
+import com.example.stepup.stats.presentation.StatsDetailsFragment
+import com.example.stepup.stats.presentation.StatsSummaryFragment
+import com.google.android.material.tabs.TabLayoutMediator
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [StatsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StatsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentStatsBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    companion object {
+        private val fragments = listOf(
+            R.string.details to { StatsDetailsFragment() },
+            R.string.summary to { StatsSummaryFragment() }
+        )
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stats, container, false)
+        _binding = FragmentStatsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StatsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StatsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val statsPageAdapter = StatsPageAdapter(this)
+        binding.pager.adapter = statsPageAdapter
+        TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
+            val tabTitleRes = fragments[position].first
+            tab.text = getString(tabTitleRes)
+            tab.setContentDescription(tabTitleRes)
+        }.attach()
+    }
+
+    class StatsPageAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+        override fun getItemCount(): Int = fragments.size
+        override fun createFragment(position: Int): Fragment = fragments[position].second()
     }
 }
