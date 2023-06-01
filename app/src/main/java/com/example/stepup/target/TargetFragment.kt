de77a6490278ed5d9c78dc6286f27f9465f3d53f
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-06-01 20:56:32 +0530

    Implement Target Preferences

--- app/src/main/java/com/example/stepup/target/TargetFragment.kt
+++ app/src/main/java/com/example/stepup/target/TargetFragment.kt
@@ -1,18 +1,51 @@
 package com.example.stepup.target
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.text.InputType
+import androidx.fragment.app.activityViewModels
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 import com.example.stepup.R
 
-class TargetFragment : Fragment() {
+class TargetFragment : PreferenceFragmentCompat() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_target, container, false)
+    private val viewModel: TargetViewModel by activityViewModels { TargetViewModel }
+//    private var _binding: FragmentTargetBinding? = null
+//    private val binding get() = _binding!!
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        _binding = FragmentTargetBinding.inflate(inflater, container, false)
+//        return binding.root
+//    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.observeTargetChanges()
+    }
+
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        _binding = null
+//    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.target, rootKey)
+
+        val dailyGoalPreference = preferenceManager.findPreference<EditTextPreference>("daily_goal")
+        dailyGoalPreference?.summaryProvider = Preference.SummaryProvider<EditTextPreference> {
+            val dailyGoal = it.text?.toIntOrNull() ?:0
+            resources.getQuantityString(R.plurals.daily_goal_summary, dailyGoal, dailyGoal)
+        }
+
+        val numericPreferenceKey = listOf("daily_goal", "step_length", "height", "weight")
+        numericPreferenceKey.forEach {
+            val preference = preferenceManager.findPreference<EditTextPreference>(it)
+            preference?.setOnBindEditTextListener { editText ->
+                editText.inputType = InputType.TYPE_CLASS_NUMBER
+            }
+        }
     }
 }
--- app/src/main/java/com/example/stepup/target/TargetViewModel.kt
+++ app/src/main/java/com/example/stepup/target/TargetViewModel.kt
@@ -1,7 +1,54 @@
 package com.example.stepup.target
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.stepup.StepUpApplication
+import com.example.stepup.core.data.repository.DayRepositoryImpl
+import com.example.stepup.core.domain.model.DayTarget
+import com.example.stepup.target.data.repository.TargetRepositoryImpl
+import com.example.stepup.target.domain.usecase.TargetUseCases
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import java.time.LocalDate
 
-class TargetViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class TargetViewModel(
+    private val targetUseCases: TargetUseCases
+) : ViewModel() {
+
+    private var observeTargetChangesJob: Job? = null
+
+    fun observeTargetChanges() {
+        observeTargetChangesJob?.cancel()
+        observeTargetChangesJob = targetUseCases.getTarget().onEach {
+            targetUseCases.updateDayTarget(
+                DayTarget(
+                    date = LocalDate.now(),
+                    goal = it.dailyGoal,
+                    height = it.height,
+                    weight = it.weight,
+                    stepLength = it.stepLength,
+                    pace = it.pace
+                )
+            )
+        }.launchIn(viewModelScope)
+    }
+
+    // TODO
+    companion object Factory : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+            val application = checkNotNull(extras[APPLICATION_KEY]) as StepUpApplication
+
+            val targetRepository = TargetRepositoryImpl(application.targetStore)
+            val dayRepository = DayRepositoryImpl(application.stepUpDatabase.dayDao)
+
+            val targetUseCases = TargetUseCases(targetRepository, dayRepository)
+
+            return TargetViewModel(targetUseCases) as T
+        }
+    }
 }
