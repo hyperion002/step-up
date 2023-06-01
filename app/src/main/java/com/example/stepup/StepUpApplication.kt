3c7c61b483ebce807f89c533c138555d18be31fa
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-06-01 20:55:44 +0530

    Initialized Database Instance

--- app/src/main/java/com/example/stepup/StepUpApplication.kt
+++ app/src/main/java/com/example/stepup/StepUpApplication.kt
@@ -1,6 +1,28 @@
 package com.example.stepup
 
 import android.app.Application
+import androidx.preference.PreferenceManager
+import androidx.room.Room
+import com.example.stepup.core.data.source.StepUpDatabase
+import com.example.stepup.target.data.source.TargetStore
+import com.example.stepup.target.data.source.TargetStoreImpl
 
+// TODO
 class StepUpApplication: Application() {
+
+    lateinit var targetStore: TargetStore
+    lateinit var stepUpDatabase: StepUpDatabase
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        targetStore = TargetStoreImpl(sharedPreferences)
+
+        stepUpDatabase = Room.databaseBuilder(
+            applicationContext,
+            StepUpDatabase::class.java,
+            StepUpDatabase.DATABASE_NAME
+        ).build()
+    }
 }
