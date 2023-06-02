5c7e80dc23718aefe065fd5f52c823e60b041ff6
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-06-02 15:10:59 +0530

    Implement Material 3

--- app/src/main/java/com/example/stepup/StepUpApplication.kt
+++ app/src/main/java/com/example/stepup/StepUpApplication.kt
@@ -1,11 +1,16 @@
 package com.example.stepup
 
 import android.app.Application
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.preference.PreferenceManager
 import androidx.room.Room
 import com.example.stepup.core.data.source.StepUpDatabase
 import com.example.stepup.target.data.source.TargetStore
 import com.example.stepup.target.data.source.TargetStoreImpl
+import com.google.android.material.color.DynamicColors
 import kotlinx.coroutines.flow.MutableStateFlow
 import java.time.LocalDate
 
@@ -19,6 +24,10 @@ class StepUpApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
+        DynamicColors.applyToActivitiesIfAvailable(this)
+        PreferenceManager.setDefaultValues(this, R.xml.target, false)
+        registerMidnightTimer()
+
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
         targetStore = TargetStoreImpl(sharedPreferences)
 
@@ -27,5 +36,23 @@ class StepUpApplication: Application() {
             StepUpDatabase::class.java,
             StepUpDatabase.DATABASE_NAME
         ).build()
+    }
+
+    private fun registerMidnightTimer() {
+        val intentFilter = IntentFilter().apply {
+            addAction(Intent.ACTION_TIME_TICK)
+            addAction(Intent.ACTION_TIME_CHANGED)
+            addAction(Intent.ACTION_TIMEZONE_CHANGED)
+        }
+        registerReceiver(midnightBroadcastReceiver, intentFilter)
+    }
+
+    private val midnightBroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val today = LocalDate.now()
+            if (today != currentDate.value) {
+                currentDate.value = today
+            }
+        }
     }
 }
