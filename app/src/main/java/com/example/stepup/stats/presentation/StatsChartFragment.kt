bcca59b600df515454b21a27eba8d89bb82dbf96
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-06-02 09:40:23 +0530

    Implement Stats

--- app/src/main/java/com/example/stepup/stats/presentation/StatsChartFragment.kt
+++ app/src/main/java/com/example/stepup/stats/presentation/StatsChartFragment.kt
@@ -4,18 +4,55 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.stepup.databinding.FragmentStatsChartBinding
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
 
 class StatsChartFragment : Fragment() {
     private var _binding: FragmentStatsChartBinding? = null
     private val binding get() = _binding!!
 
+    private val statsDetailsViewModel: StatsDetailsViewModel by activityViewModels { StatsDetailsViewModel }
+    private lateinit var chartPageAdapter: ChartPageAdapter
+
+    private val dateFormatter = DateTimeFormatter.ofPattern("EEE, MMM, dd")
+
+    class ChartPageAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+        var dateRange = LocalDate.now()..LocalDate.now()
+
+        fun getPageContaining(selectedDate: LocalDate): Int {
+            val period = Period.between(selectedDate, dateRange.endInclusive)
+            return (period.days / 7).coerceIn(0, itemCount)
+        }
+        override fun getItemCount(): Int = dateRange.run {
+            val period = Period.between(start, endInclusive)
+            return period.days / 7 + 1
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            val fragment = StatsChartPageFragment()
+            fragment.arguments = Bundle().apply {
+                putLong(StatsChartPageFragment.ARG_PAGE_NUMBER, position.toLong())
+            }
+            return fragment
+        }
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         _binding = FragmentStatsChartBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -25,5 +62,41 @@ class StatsChartFragment : Fragment() {
         _binding = null
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        chartPageAdapter = ChartPageAdapter(this)
+        binding.viewPagerChart.adapter = chartPageAdapter
+
+        binding.buttonPreviousDay.setOnClickListener { changeSelectedDate(offset = -1) }
+        binding.buttonNextDay.setOnClickListener { changeSelectedDate(offset = 1) }
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                statsDetailsViewModel.day.collect {
+                    updateUserInterface(it.date, it.chartDateRange)
+                }
+            }
+        }
+    }
+
+    private fun updateUserInterface(selectedDate: LocalDate, dateRange: ClosedRange<LocalDate>) {
+        binding.apply {
+            textSelectedDate.text = selectedDate.format(dateFormatter)
+            buttonPreviousDay.isVisible = selectedDate.isAfter(dateRange.start)
+            buttonNextDay.isVisible = selectedDate.isBefore(dateRange.endInclusive)
+            chartPageAdapter.dateRange = dateRange
+            scrollChartTo(selectedDate)
+        }
+    }
+
+    private fun scrollChartTo(selectedDate: LocalDate) {
+        val pageIndex = chartPageAdapter.getPageContaining(selectedDate)
+        binding.viewPagerChart.currentItem = pageIndex
+    }
+
+    private fun changeSelectedDate(offset: Long) {
+        val currentDate = statsDetailsViewModel.day.value.date
+        statsDetailsViewModel.selectDay(currentDate.plusDays(offset))
+    }
 }
